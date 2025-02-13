65eba7c17037a3a1c012cdc45b75761b14f9d188
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-02-13 11:58:42 +0530

    oop

--- Lab experiments/6Feb_codes/CountOccurences.java
+++ Lab experiments/6Feb_codes/CountOccurences.java
@@ -1,13 +1,9 @@
 public class CountOccurences {
 
     public static void main(String[] args) {
-
         int[] array = {2, 3, 2, 5, 2, 6};
-
         int target = 2;
-
         int count = 0;
-
         for (int num : array) {   // for-each loop is used to iterate through each element of array
                                   // num takes the value of each element in array one by one.
             if (num == target) {
