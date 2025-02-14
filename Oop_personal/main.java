dcf4aeb4f56c83a0b0dfb8a0eaf0aa7b08fdbddb
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-02-15 02:14:43 +0530

    oop

---
+++ Oop_personal/main.java
@@ -0,0 +1,49 @@
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("Writing");
+    }
+    public void printColor() {
+        System.out.println(this.color);
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+
+
+public class main {
+    public static void main(String[] args) {
+        /* 
+        Pen p1 = new Pen();
+        p1.color = "Red";
+        p1.type = "Ballpoint";
+
+        // System.out.println(p1.color);
+        // System.out.println(p1.type);
+        p1.write();
+        p1.printColor();
+
+        Pen p2 = new Pen();
+        p2.color = "Blue";
+        p2.type = "Gel";
+        p2.printColor();
+        */
+
+        Student s1 = new Student();
+        s1.name = "John";
+        s1.age = 20;
+
+        s1.printInfo();
+
+    }
+}
---
+++ Oops_Durga/hello.java
@@ -0,0 +1,18 @@
+class D {
+    public static void main(String[] args) {
+        System.out.println("hello japan");
+    }
+}
+class C {
+    public static void main(String[] args) {
+        System.out.println("hello usa");
+    }
+}
+class hello {
+    public static void main(String[] args) {
+        System.out.println("hello turkey");
+    }
+}
+class A {
+}
+
