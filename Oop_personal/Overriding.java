999b0d4378738cabbc063293b1b902bae1d30a30
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-02-16 19:07:28 +0530

    oop

---
+++ Oop_personal/Overriding.java
@@ -0,0 +1,21 @@
+class A {
+    public int a;
+    public int harry() {
+        return 4;
+    }
+    public void meth2() {
+        System.out.println("I am a method 2 of class A");
+    }
+}
+class B extends A{
+    public void meth3() {
+        System.out.println("I am method 3 of class A");
+    }
+}
+
+
+public class Overriding {
+    public static void main(String[] args) {
+        
+    }                                                
+}
