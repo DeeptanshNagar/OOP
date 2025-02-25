8fc922e195f2cf1e7e4d651889a688e5dbfeef7a
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-02-26 00:40:54 +0530

    oops

--- Abstraction.java
+++ Abstraction.java
@@ -1,40 +1,27 @@
-// Abstract Class: Animal
 abstract class Animal {
-    String name;
-
-    // Constructor for Animal class
+    String name; 
     Animal(String name) {
         this.name = name;
     }
-
-    // Abstract method (no implementation)
     abstract void makeSound();
 
-    // Concrete method (implementation provided)
     void eat() {
-        System.out.println(name + " is eating");
+        System.out.println(name + " is eating !! ");
     }
 }
 
-// Subclass: Dog
-class Dog extends Animal {
-    // Constructor for Dog class
+class Dog extends Animal{
     Dog(String name) {
         super(name);
     }
-
-    // Implementation of abstract method
     void makeSound() {
-        System.out.println(name + " barks");
+        System.out.println(name + " is barking...");
     }
 }
-
-// Main class to demonstrate abstraction
-public class Abstraction {
+public class Abstraction{
     public static void main(String[] args) {
-        // Abstract class reference, Dog object
-        Animal dog = new Dog("Buddy");
-        dog.makeSound(); // Output: Buddy barks
-        dog.eat();       // Output: Buddy is eating
+        Dog d = new Dog("sandy");
+        d.eat();
+        d.makeSound();
     }
 }
