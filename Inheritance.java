f05f34b2a5dc928d33c0ba24b9ac5bba03810667
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-02-25 15:12:36 +0530

    oop

--- Inheritance.java
+++ Inheritance.java
@@ -8,12 +8,7 @@ class Animal {
 
     // Method to make a sound (can be overridden by subclasses)
     void makeSound() {
-        System.out.println(name + " makes a sound");
-    }
-
-    // Display animal's name
-    void display() {
-        System.out.println("This is an animal named: " + name);
+        System.out.println("Animal makes a sound");
     }
 }
 
@@ -29,49 +24,11 @@ class Dog extends Animal {
     void makeSound() {
         System.out.println(name + " barks");
     }
-
-    // Additional method specific to Dog
-    void fetch() {
-        System.out.println(name + " is fetching a ball!");
-    }
-}
-
-class Cat extends Animal {
-
-    // Constructor
-    Cat(String name) {
-        super(name);
-    }
-
-    // Overriding the makeSound method
-    @Override
-    void makeSound() {
-        System.out.println(name + " meows");
-    }
-
-    // Additional method specific to Cat
-    void scratch() {
-        System.out.println(name + " is scratching the furniture!");
-    }
 }
 
 public class Inheritance {
     public static void main(String[] args) {
-        // Create Animal objects
-        Animal genericAnimal = new Animal("Generic Animal");
-        genericAnimal.makeSound();
-        genericAnimal.display();
-
-        // Create Dog object
-        Dog dog = new Dog("Buddy ");
-        dog.makeSound();
-        dog.display();
-        dog.fetch();
-
-        // Create Cat object
-        Cat cat = new Cat("Whiskers ");
-        cat.makeSound();
-        cat.display();
-        cat.scratch();
+        Dog myDog = new Dog("Rover");
+        myDog.makeSound();
     }
 }
