ecf3a431da3ab2f47afc49fdbcf410372053c859
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-04-04 13:56:57 +0530

    OopSsS

--- TheoryAssessment/EventDemo.java
+++ TheoryAssessment/EventDemo.java
@@ -2,21 +2,23 @@ interface EventHandler {
     void handleEvent();
 }
 
-class EventDemo {
+public class EventDemo {
     public void registerEvent() {
         class LocalEventHandler implements EventHandler {
+            @Override
             public void handleEvent() {
                 System.out.println("Event handled by local inner class");
             }
         }
-        EventHandler localHandler = new LocalEventHandler();
+        LocalEventHandler localHandler = new LocalEventHandler();
         localHandler.handleEvent();
     }
-
+    
     public static void main(String[] args) {
         EventDemo demo = new EventDemo();
         demo.registerEvent();
         EventHandler anonymousHandler = new EventHandler() {
+            @Override
             public void handleEvent() {
                 System.out.println("Event handled by anonymous inner class");
             }
@@ -24,4 +26,3 @@ class EventDemo {
         anonymousHandler.handleEvent();
     }
 }
-
